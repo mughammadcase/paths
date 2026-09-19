b22ba80b9d2c511f2f3d601ce8ac169ef15d8806
Mughammad <caseameen44@gmail.com>
2026-09-19 04:47:02 +0200

    feat: analyze function parameters

--- src/analyzer/analyzer.test.js
+++ src/analyzer/analyzer.test.js
@@ -6,12 +6,21 @@ describe("analyzeSource", () => {
   // the analyzer should extract the names of top level functions
   it("extracts function declarations from the AST", () => {
     const source = `
-    function login() {}
-    function logout() {}`;
+    function login(email, password) {}
+    function logout(username) {}`;
 
     const ast = parseSource(source);
     const functions = analyzeSource(ast);
 
-    expect(functions).toEqual(["login", "logout"]);
+    expect(functions).toEqual([
+      {
+        name: "login",
+        parameters: ["email", "password"],
+      },
+      {
+        name: "logout",
+        parameters: ["username"],
+      },
+    ]);
   });
 });
